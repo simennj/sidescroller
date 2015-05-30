f2af94ef65101048e95a58d5c7c2c7b2107260f9
unknown <simennj@outlook.com>
2015-05-30 23:43:42 +0200

    asdf

---
+++ game/desktop/src/com/ostepropp/sidescroller/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ostepropp.sidescroller.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ostepropp.sidescroller.GameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClass(), config);
+	}
+}

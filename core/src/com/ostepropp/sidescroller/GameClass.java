774f46fa640c78c24c700cd81b5cc3e967c4b7cc
Simen <simennj@outlook.com>
2015-06-01 20:31:39 +0200

    add menu

--- core/src/com/ostepropp/sidescroller/GameClass.java
+++ core/src/com/ostepropp/sidescroller/GameClass.java
@@ -3,6 +3,7 @@ package com.ostepropp.sidescroller;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 import com.ostepropp.sidescroller.screens.GameScreen;
+import com.ostepropp.sidescroller.screens.MenuScreen;
 
 public class GameClass extends Game {
 	
@@ -12,6 +13,11 @@ public class GameClass extends Game {
 	@Override
 	public void create () {
 		gameScreen = new GameScreen();
+		menuScreen = new MenuScreen(this);
+		setScreen(menuScreen);
+	}
+	
+	public void startGame() {
 		setScreen(gameScreen);
 	}
 }
--- core/src/com/ostepropp/sidescroller/screens/MenuScreen.java
+++ core/src/com/ostepropp/sidescroller/screens/MenuScreen.java
@@ -1,42 +1,89 @@
 package com.ostepropp.sidescroller.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.ostepropp.sidescroller.GameClass;
 
-public class MenuScreen implements Screen{
 
+public class MenuScreen implements Screen {
+
+	GameClass game;
+	Stage stage;
+	Skin skin;
+	Table table;
+	
+	public MenuScreen(GameClass game) {
+		this.game = game;
+	}
+	
 	@Override
 	public void show() {
-		
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
+		skin = new Skin(Gdx.files.internal("uiskin.json"));
+		table = new Table(skin);
+		table.setFillParent(true);
+		table.center();
+		TextButton start = new TextButton("start", skin);
+		start.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				game.startGame();
+				System.out.println("asdf");
+			}
+		});
+		table.add(new Label("ShapeRenderer er BEST", skin));
+		table.row();
+		table.add(start);
+		table.setDebug(true);
+		stage.addActor(table);
 	}
 
 	@Override
 	public void render(float delta) {
-		
+	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	    stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		
+		stage.getViewport().update(width, height, true);
 	}
 
 	@Override
 	public void pause() {
-		
+
 	}
 
 	@Override
 	public void resume() {
-		
+
 	}
 
 	@Override
 	public void hide() {
-		
+
 	}
 
 	@Override
 	public void dispose() {
-		
+
 	}
 
 }
